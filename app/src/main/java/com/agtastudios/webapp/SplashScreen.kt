6a4adc4d7e0a8464ae86509358ab970b1b0a3325
agta <angeloabrita@gmail.com>
2023-03-13 20:43:49 -0300

    fix bugs

--- app/src/main/java/com/agtastudios/webapp/SplashScreen.kt
+++ app/src/main/java/com/agtastudios/webapp/SplashScreen.kt
@@ -22,7 +22,7 @@ class SplashScreen : AppCompatActivity() {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
-        }, 3000)
+        }, 1500)
     }
 
     private fun animateImage() {
